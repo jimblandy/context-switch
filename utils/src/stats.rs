b6926536e19fc6b59b5eb2ad2060295ecbebca6b
Jim Blandy <jimb@red-bean.com>
2019-11-29 17:46:40 -0800

    stats: Implement Extend. Add a test.

--- utils/src/stats.rs
+++ utils/src/stats.rs
@@ -24,3 +24,18 @@ impl Stats {
         (self.x0s as f64 * self.x2s - self.x1s * self.x1s).sqrt() / self.x0s as f64
     }
 }
+
+impl Extend<f64> for Stats {
+    fn extend<T: IntoIterator<Item=f64>>(&mut self, iter: T) {
+        iter.into_iter().for_each(|x| self.push(x));
+    }
+}
+
+#[test]
+fn pop_stddev() {
+    let mut stats = Stats::new();
+
+    stats.extend([2.0_f64, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0].iter().copied());
+    assert_eq!(stats.mean(), 5.0);
+    assert_eq!(stats.population_stddev(), 2.0);
+}
